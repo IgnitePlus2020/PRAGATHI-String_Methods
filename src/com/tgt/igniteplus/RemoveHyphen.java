6810d4bd27359112b91c0708790baa1997b1fd8f
pragathiindran <50520367+pragathiindran@users.noreply.github.com>
2020-05-03 20:27:04 +0530

    Update RemoveHyphen.java

--- src/com/tgt/igniteplus/RemoveHyphen.java
+++ src/com/tgt/igniteplus/RemoveHyphen.java
@@ -1,27 +1,34 @@
 //program to remove hyphen(-) from a given string only if it is inbetween digits
 package com.tgt.igniteplus;
 import java.util.Scanner;
-public class RemoveHyphen {
+public class RemoveHyphen
+ {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string : ");
-        String str = sc.nextLine();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String str= in.nextLine();
         char[] s = str.toCharArray();
-        char[] newStringArray = new char[str.length()];
-        int i, j = 0;
-        for (i = 0; i < str.length(); i++) {
-            if ((s[i] == '-') && (s[i - 1]<65) && (s[i + 1]<65));
-            else {
-                newStringArray[j] = s[i];
-                j++;
-            }
+        char[] newStr = new char[str.length()];
+        int j=0,i;
+        newStr[j++]=s[0];
+        for(i=1;i<str.length();i++)
+        {
+            if(s[i] == '-' && s[i-1]<65 && s[i+1]<65);
+            else
+                newStr[j++]= s[i];
         }
-        System.out.print("String after removing hyphen : ");
-        System.out.print(newStringArray);
+
+        System.out.print("The string after hyphen removal is ");
+        System.out.println(newStr);
     }
+
 }
 /*OUTPUT:
-Enter the string : 
+Enter a string
 134-10/5566 A-block,Manyata Tech-Park
-String after removing hyphen : 13410/5566 A-block,Manyata Tech-Park 
+The string after hyphen removal is 13410/5566 A-block,Manyata Tech-Park 
+
+Enter a string
+-134-10/5566 A-block, New Manyata Tech-Park 12-45 testing
+The string after hyphen removal is -13410/5566 A-block, New Manyata Tech-Park 1245 testing
  */
